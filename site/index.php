a056ba583cbab2084ba785fc1c009f186c5a968e
Leonardo Gomes Da Silva <35742346+LeonardoGomes99@users.noreply.github.com>
2020-04-18 22:16:51 -0300

    att
    att

---
+++ site/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+    <head>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" href="css/index.css">
+    </head>
+    <body>
+        <div class="sidenav">
+            <a href="#about">Desejar Doar?</a>
+            <hr>
+            <a href="#services">Contato</a>
+            <hr>
+            <a href="#clients">Feedback</a>
+            <hr>
+            <a href="#contact">Sobre Nós</a>
+            <hr>
+        </div>
+        <div class="Background">
+            <div class="BackGroundPost">
+                <img class="imagespost" src="http://www.cdc.gov/stroke/images/family_history.jpg">
+            </div>
+        </div>
+    </body>
+</html>

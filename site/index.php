2e891159440215d66f5487974284893182a0f938
SotnDragon <jonatapqt01@gmail.com>
2020-04-19 17:14:03 -0300

    finalizando

--- site/index.php
+++ site/index.php
@@ -22,10 +22,29 @@
         background-repeat: no-repeat;
     }
 
+
     .text {
         background-color: rgba(200, 200, 200, .5);
         border-radius: 5px;
 
+    }
+
+    body {
+        background-color: #E9EBEE;
+    }
+
+    @media only screen and (max-width: 600px) {
+        .font-weight-light {
+            font-size: 18px;
+        }
+
+        .fonte {
+            font-size: 13px;
+        }
+
+        .text {
+            background-color: rgba(200, 200, 200, .3);
+        }
     }
 </style>
 
@@ -40,16 +59,19 @@
                         <div class="col-md-8 col-sm-12 text">
                             <h1 class="font-weight-light">Olá, bem-vindo(a) ao</h1>
                             <h1 class="font-weight-light">Programa - Corrente Do Bem</h1>
-                            <p class="text-justify">
+                            <p class="text-justify fonte">
                                 A gentiliza é um ato de amor ao próximo e ao Criador. Uma linda estrada de doação e carinho. Não é difícil ser gentil, é uma entrega constante e a essência para receber bons frutos, além desse retorno, ela gera humildade e gratidão. Quem pratica esse belo ato, colhe sorrisos, respeito, amor e infinitas lembranças de gestos inesperados de solidariedade. É a extensão do amor de Deus e um afago na alma. Gentileza sua linda seja sempre bem vinda!
+                            </p>
+                            <p class="fonte">
+                                Pensando nisso, criamos o Projeto Corrente do Bem que tem por objetivo a doação e atenção aos mais necessitados neste momento de pandemia.
                             </p>
                         </div>
                         <div class="col-md-4 col-sm-12">
-                            <div class="mt-5">                            
-                                    <button onclick="window.location.href = 'doacao.php';" class="btn btn-lg btn-success btn-block">Seja um doador</button>                                
+                            <div class="mt-5">
+                                <button onclick="window.location.href = 'doacao.php';" class="btn btn-lg btn-success btn-block fonte">Seja um doador</button>
                             </div>
                             <div class="mt-5">
-                                <button onclick="window.location.href = 'beneficiario.php';" class="btn btn-lg btn-primary btn-block">Realizar Solicitação</button>
+                                <button onclick="window.location.href = 'beneficiario.php';" class="btn btn-lg btn-primary btn-block fonte">Realizar Solicitação</button>
                             </div>
                         </div>
                     </div>
@@ -58,6 +80,39 @@
             </div>
         </div>
     </header>
+
+
+    <!-- Page Content -->
+    <section class="py-5">
+        <div class="container">
+            <h2 class="font-weight-light">Saiba mais</h2>
+
+        </div>
+    </section>
+
+
+    <div class="container">
+        <div class="col-12">
+            <div class="row">
+                <div class="col-md-7 col-sm-12">
+                    <iframe width="100%" height="315" src="https://www.youtube.com/embed/YKD8EJQXoXw" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                </div>
+                <div class="col-md-5 col-sm-12 ">
+                    <p class="text-justify">
+                        Somos uma equipe formada por 6 integrantes que visam o melhor estado da sociedade, respeitando sempre a ética e a vida. Este projeto tem como intuito promover a solidariedade e empatia para com o próximo visando construir uma sociedade mais humana e atenciosa com os mais necessitados.
+                    </p>
+                    <p>
+                        Quer juntar-se a nossa equipe?
+                    </p>
+                    <p>
+                        Envie-nos uma mensagem apresentando-se para correntedobem.sont@gmail.com
+                    </p>
+
+
+                </div>
+            </div>
+        </div>
+    </div>
 
     <br>
 </body>

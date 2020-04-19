6e7e31aa4ec8779823f6a9475b3a66cac37fa05d
Leonardo Gomes Da Silva <35742346+LeonardoGomes99@users.noreply.github.com>
2020-04-18 22:47:38 -0300

    att
    att

--- site/index.php
+++ site/index.php
@@ -2,7 +2,7 @@
 <html>
     <head>
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" type="text/css" href="src/index.css">
+    <link rel="stylesheet" type="text/css" href="css/index.css">
     </head>
     <body>
         <div class="sidenav">
@@ -16,9 +16,26 @@
             <hr>
         </div>
         <div class="Background">
+
+
             <div class="BackGroundPost">
-                <img class="imagespost" src="http://www.cdc.gov/stroke/images/family_history.jpg">
+
+
+                <div class="Title">
+                    <p>DOAÇÃO Nº1</p>
+                </div>
+
+                <div class="divbox-image">
+                    <img class="imagespost" src="src/indexbackground.jpg">
+                </div>
+
+                <div class="div-post">
+                    <p>A gentiliza é um ato de amor ao próximo e ao Criador. Uma linda estrada de doação e carinho. Não é difícil ser gentil, é uma entrega constante e a essência para receber bons frutos, além desse retorno, ela gera humildade e gratidão. Quem pratica esse belo ato, colhe sorrisos, respeito, amor e infinitas lembranças de gestos inesperados de solidariedade. É a extensão do amor de Deus e um afago na alma.
+                    Gentileza sua linda seja sempre bem vinda!</p>
+                </div>
+
             </div>
+            
         </div>
     </body>
 </html>
